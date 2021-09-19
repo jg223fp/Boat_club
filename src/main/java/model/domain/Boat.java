3aaeca625f1eb3360346ca280bba2e49df003303
johan <jg223fp@student.lnu.se>
2021-09-19 21:49:46 +0200

    Changed indetationlevel

--- src/main/java/model/domain/Boat.java
+++ src/main/java/model/domain/Boat.java
@@ -1,33 +1,49 @@
 package model.domain;
 
 public class Boat {
+		
+	private String name;
+	private Enum boatType;
+	private float length;
+	
+	/**
+	* Returns members name.
+	*/
+	private String getName() {
+		return this.name;
+	}
 
-    private String name;
-    private Enum boatType;
-    private float length;
-    
-    
-    private String getName() {
-        return this.name;
-    }
+	/**
+	* @generated
+	*/
+	private void setName(String name) {
+		this.name = name;
+	}
 
-    private void setName(String name) {
-        this.name = name;
-    }
+	/**
+	* @generated
+	*/
+	private Enum getBoatType() {
+		return this.boatType;
+	}
+	/**
+	* @generated
+	*/
+	private void setBoatType(Enum boatType) {
+		this.boatType = boatType;
+	}
 
-    private Enum getBoatType() {
-        return this.boatType;
-    }
+	/**
+	* @generated
+	*/
+	private float getLength() {
+		return this.length;
+	}
 
-    private void setBoatType(Enum boatType) {
-        this.boatType = boatType;
-    }
-
-    private float getLength() {
-        return this.length;
-    }
-
-    private void setLength(float length) {
-        this.length = length;
-    }
+	/**
+	* @generated
+	*/
+	private void setLength(float length) {
+		this.length = length;
+	}
 }
