854c1670f4980574e0bc2769aedbf99450108911
johan <jg223fp@student.lnu.se>
2021-09-20 18:37:02 +0200

    Added app name and created user object

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -1,11 +1,20 @@
 package controller;
 
+import model.domain.MemberRegistry;
 
 /**
 * A class to initiate the application.
 */
 public class App {
   
-  private String appName;
+  private String appName = "Boatclub system v1.0";
 
+  public static void main(String[] args) {
+
+    MemberRegistry mReg = new MemberRegistry(); 
+    User user = new User(mReg, appName);
+    user.runApp();                                //initiates app
+  }
 }
+
+
