971e9e256f1e51a821cc757ffa7df836379ff94c
johan <jg223fp@student.lnu.se>
2021-09-29 20:14:26 +0200

    Added method for getting number of members

--- src/main/java/model/domain/MemberRegistry.java
+++ src/main/java/model/domain/MemberRegistry.java
@@ -3,22 +3,22 @@ package model.domain;
 import java.util.ArrayList;
 
 /**
-* A class that stores all member objects.
-*/
+ * A class that stores all member objects.
+ */
 public class MemberRegistry {
-    
+
   private ArrayList<Member> members;
-  
+
   /**
-  * Class constructor.
-  */
+   * Class constructor.
+   */
   public MemberRegistry() {
     this.members = new ArrayList<Member>();
   }
-  
+
   /**
-  * Creates a new member and returns the member ID.
-  */
+   * Creates a new member and returns the member ID.
+   */
   public int addMember(String firstName, String lastName, long personalNumber) {
     int memberId = generateMemberId();
     Member m = new Member(firstName, lastName, personalNumber, memberId);
@@ -27,39 +27,47 @@ public class MemberRegistry {
   }
 
   /**
-  * Deletes a member.
-  */
+   * Deletes a member.
+   */
   public void deleteMember(int memberId) {
     for (Member m : members) {
       if (m.getMemberId() == memberId) {
         members.remove(m);
         break;
       }
-    }  
+    }
   }
-  
+
   /**
-  * Returns the member list.
-  */
+   * Returns the member list.
+   */
   public Iterable<Member> getMemberList() {
     return members;
   }
-  
+
   /**
-  * Returns a member object from a given memberId.
-  */
+   * Returns the number of members.
+   */
+  public int getNumberOfMembers() {
+    int number = members.size();
+    return number;
+  }
+
+  /**
+   * Returns a member object from a given memberId.
+   */
   public Member getMember(int memberId) {
     for (Member m : members) {
       if (m.getMemberId() == memberId) {
         return m;
       }
-    } 
+    }
     return null;
   }
-  
+
   /**
-  * Generates a member ID for a new member.
-  */
+   * Generates a member ID for a new member.
+   */
   private int generateMemberId() {
     int memberId = 0;
     for (Member m : members) {
@@ -68,5 +76,5 @@ public class MemberRegistry {
       }
     }
     return memberId + 1;
-  } 
+  }
 }
